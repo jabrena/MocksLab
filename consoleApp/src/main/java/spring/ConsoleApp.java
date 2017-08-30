7b44b235eb6f60b4bb8c8826c6b0b8ef3c2a6431
Juan Antonio Breña Moral <bren@juanantonio.info>
2017-08-30 12:51:38 +0200

    Connecting Service on App

--- consoleApp/src/main/java/spring/ConsoleApp.java
+++ consoleApp/src/main/java/spring/ConsoleApp.java
@@ -1,25 +1,36 @@
 package spring;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class ConsoleApp {
+public @Slf4j class ConsoleApp {
+
+    private SimpleService simpleService;
+
+    public ConsoleApp(SimpleService simpleService){
+        this.simpleService = simpleService;
+    }
 
     public static void main(String[] args){
 
-        ConfigurableApplicationContext context = new SpringApplicationBuilder()
-                .sources(ConsoleApp.class)
-                .run(args);
-
-        ConsoleApp app = context.getBean(ConsoleApp.class);
-        app.start();
+        SpringApplication app = new SpringApplication(ConsoleApp.class);
+        app.run(args);
 
     }
 
-    private void start() {
-        System.out.println("Hello World!");
+    private void run(String[] args) {
+
+        LOGGER.info("Running");
+
+        if (args.length > 0) {
+            LOGGER.info(simpleService.hello(args[0].toString()));
+        } else {
+            LOGGER.info(simpleService.hello("world"));
+        }
+
+        System.exit(0);
     }
 
 }
--- consoleApp/src/test/java/spring/SimpleServiceSystemTest.java
+++ consoleApp/src/test/java/spring/SimpleServiceSystemTest.java
@@ -5,26 +5,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ConsoleAppConfiguration.class)
 public @Slf4j class SimpleServiceSystemTest {
-
-    @Configuration
-    static class SimpleServiceConfiguration {
-
-        @Bean
-        public SimpleService simpleService() {
-            return new SimpleServiceImpl();
-        }
-
-    }
 
     @Autowired
     SimpleService simpleService;
